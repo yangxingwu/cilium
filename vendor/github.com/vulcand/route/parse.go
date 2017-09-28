b60441638ef5d285c261aceccb7cd5c284524311
Manali Bhutiyani <manali@covalent.io>
2017-09-28 11:22:07 -0700

    changes in parser in vulcand lib

--- vendor/github.com/vulcand/route/parse.go
+++ vendor/github.com/vulcand/route/parse.go
@@ -12,8 +12,46 @@ func IsValid(expr string) bool {
 	return err == nil
 }
 
+/*
+// IsValid checks whether expression is valid
+func IsKafkaValid(expr string) bool {
+	_, err := parseKafka(expr, &match{})
+	return err == nil
+}
 
 //Manali TODO new parser here for kafka.
+
+func parseKafka(expression string, result *match) (matcher, error) {
+	p, err := predicate.NewParser(predicate.Def{
+		Functions: map[string]interface{}{
+			"ApiVersion":       methodTrieMatcher,
+			"ApiVersionRegexp": methodRegexpMatcher,
+
+			"Topic":       methodTrieMatcher,
+			"TopicRegexp": methodRegexpMatcher,
+
+			"ApiKey":       methodTrieMatcher,
+			"ApiKeyRegexp": methodRegexpMatcher,
+		},
+		Operators: predicate.Operators{
+			AND: newAndMatcher,
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+	out, err := p.Parse(expression)
+	if err != nil {
+		return nil, err
+	}
+	m, ok := out.(matcher)
+	if !ok {
+		return nil, fmt.Errorf("unknown result type: %T", out)
+	}
+	m.setMatch(result)
+	return m, nil
+}
+*/
 
 func parse(expression string, result *match) (matcher, error) {
 	p, err := predicate.NewParser(predicate.Def{
