6d2385808b032d960f93ba292113d4c724e3fe15
Romain Lenglet <romain@covalent.io>
2018-04-25 23:16:42 -0700

    identity: Allocate reserved identities for entity reserved labels
    Allocate ID 4 to "health" entities (health daemon endpoints).
    Allocate ID 5 to "init" entities (initializing endpoints).
    
    Allocate such entity IDs synchronously, since that doesn't require
    any kvstore interactions.
    
    Fixes: https://github.com/cilium/cilium/issues/3895
    Signed-off-by: Romain Lenglet <romain@covalent.io>

--- pkg/identity/allocator.go
+++ pkg/identity/allocator.go
@@ -91,6 +91,21 @@ func InitIdentityAllocator(owner IdentityAllocatorOwner) {
 	})
 }
 
+// IdentityAllocationIsLocal returns true if a call to AllocateIdentity with
+// the given labels would not require accessing the KV store to allocate the
+// identity.
+// Currently, this function returns true only if the labels are those of a
+// reserved identity, i.e. if the slice contains a single reserved
+// "reserved:*" label.
+func IdentityAllocationIsLocal(lbls labels.Labels) bool {
+	// If there only one label with the "reserved" source and a well-known key,
+	// use the well-known identity for that key.
+	if reservedIdentity := LookupReservedIdentity(lbls); reservedIdentity != nil {
+		return true
+	}
+	return false
+}
+
 // AllocateIdentity allocates an identity described by the specified labels. If
 // an identity for the specified set of labels already exist, the identity is
 // re-used and reference counting is performed, otherwise a new identity is
@@ -99,6 +114,17 @@ func AllocateIdentity(lbls labels.Labels) (*Identity, bool, error) {
 	log.WithFields(logrus.Fields{
 		logfields.IdentityLabels: lbls.String(),
 	}).Debug("Resolving identity")
+
+	// If there only one label with the "reserved" source and a well-known key,
+	// use the well-known identity for that key.
+	if reservedIdentity := LookupReservedIdentity(lbls); reservedIdentity != nil {
+		log.WithFields(logrus.Fields{
+			logfields.Identity:       reservedIdentity.ID,
+			logfields.IdentityLabels: lbls.String(),
+			"isNew":                  false,
+		}).Debug("Resolved reserved identity")
+		return reservedIdentity, false, nil
+	}
 
 	id, isNew, err := identityAllocator.Allocate(globalIdentity{lbls})
 	if err != nil {
@@ -118,5 +144,9 @@ func AllocateIdentity(lbls labels.Labels) (*Identity, bool, error) {
 // identity again. This function may result in kvstore operations.
 // After the last user has released the ID, the returned lastUse value is true.
 func (id *Identity) Release() error {
+	// Ignore reserved identities.
+	if reservedIdentityCache[id.ID] != nil {
+		return nil
+	}
 	return identityAllocator.Release(globalIdentity{id.Labels})
 }
